4e0d22dffc7792aa80d664c442aa7a5302128ada
Lars Trieloff <lars@trieloff.net>
2024-09-11 10:39:49 +0200

    feat(index): support `window.RUM_PARAMS` to attach custom URL parameters to collection URL

--- src/index.js
+++ src/index.js
@@ -60,7 +60,9 @@ export function sampleRUM(checkpoint, data) {
         sampleRUM.sendPing = (ck, time, pingData = {}) => {
           // eslint-disable-next-line max-len, object-curly-newline
           const rumData = JSON.stringify({ weight, id, referer: window.location.href, checkpoint: ck, t: time, ...pingData });
-          const { href: url, origin } = new URL(`.rum/${weight}`, sampleRUM.collectBaseURL);
+          const u = new URL(`.rum/${weight}`, sampleRUM.collectBaseURL);
+          u.searchParams = window.RUM_PARAMS || new URLSearchParams();
+          const { href: url, origin } = u;
           const body = origin === window.location.origin ? new Blob([rumData], { type: 'application/json' }) : rumData;
           navigator.sendBeacon(url, body);
           // eslint-disable-next-line no-console
