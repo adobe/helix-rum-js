4452c2d80de57a70ad30afa43e9a5ac5e06bcd86
Lars Trieloff <lars@trieloff.net>
2024-09-18 10:09:56 +0200

    fix(release): include explicit list of assets

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -7,10 +7,34 @@ module.exports = {
     }],
     "@semantic-release/npm",
     ["@semantic-release/git", {
-      "assets": ["package.json", "CHANGELOG.md", 'dist/*.md5'],
+      "assets": [
+        "package.json",
+        "CHANGELOG.md",
+        'dist/rum-standalone.md5',
+        'dist/rum-standalone-404.md5'
+      ],
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
     }],
-    ["@semantic-release/github", {}]
+    ["@semantic-release/github", {
+      "assets": [
+        {
+          "path": "dist/rum-standalone.js",
+          "label": "RUM Standalone JS"
+        },
+        {
+          "path": "dist/rum-standalone.md5",
+          "label": "RUM Standalone Hash"
+        },
+        {
+          "path": "dist/rum-standalone-404.js",
+          "label": "RUM Standalone 404 JS"
+        },
+        {
+          "path": "dist/rum-standalone-404.md5",
+          "label": "RUM Standalone 404 Hash"
+        }
+      ]
+    }]
   ],
   branches: ['main', '1.x'],
 };
