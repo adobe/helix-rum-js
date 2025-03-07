8bf841b9c82fc06cadf6f59c5e92f95615a722b2
kptdobe <acapt@adobe.com>
2025-03-07 10:54:19 +0100

    chore: try different timeout

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -10,6 +10,7 @@
  * governing permissions and limitations under the License.
  */
 export default {
+  browserStartTimeout: 10000,
   coverageConfig: {
     report: true,
     reportDir: 'coverage',
