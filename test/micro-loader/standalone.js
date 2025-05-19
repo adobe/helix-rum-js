ce805ca3e1465893168f0507822389e52290743c
Lars Trieloff <lars@trieloff.net>
2025-05-19 16:01:05 +0200

    feat: implement micro-loader for operational telemetry
    - Updated `src/index.js` to enhance RUM initialization logic.
    - Created new test files for micro-loader functionality: `optel-in.test.html`, `optel-oneline.test.html`, and `optel-out.test.html`.
    - Added `standalone.js` to simulate script loading and beacon sending for telemetry.
    - Improved test descriptions for clarity and consistency.

---
+++ test/micro-loader/standalone.js
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2025 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+/* eslint-disable */
+(function () {
+  console.log('standalone.js loaded', document.currentScript.outerHTML);
+  navigator.sendBeacon('https://rum.hlx.page/.rum/1?program=pXXXXXX&environment=eYYYYYY', '{"checkpoint":"top", "id":"superfake"}');
+}());
+// this file is a poor imitation of our dist/standalone.js
+// but it is useful to verify that the script is indeed loaded
+// with the correct parameters
